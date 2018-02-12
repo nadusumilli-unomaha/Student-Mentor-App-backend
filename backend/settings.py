e743e132591e1eb5331d66089f491f933cf5026a
Matt Hale <mlhale@unomaha.edu>
2018-02-12 17:01:11 -0600

    fixing db setup for production

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'student_db',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
